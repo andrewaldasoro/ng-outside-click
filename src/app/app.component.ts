b10caff3686fed9ba8ab8e5f49e33dc34363d4b4
Kev ALDASORO <kev-andrew.aldasoro@thalesgroup.com>
2019-08-30 22:11:35 -0500

    Reindent Lines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,22 @@
 import { Component, HostListener, ViewChild, ElementRef } from '@angular/core';
 
-
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
-	title = 'outside-click';
+    title = 'outside-click';
 
-	@ViewChild('testButton', {static: false}) testButton: ElementRef;
-	
-	@HostListener('document:click', ['$event']) onClick(event) {
-		if (this.testButton.nativeElement.contains(event.target)) {// or some similar check
-			console.log('Click Inside');
-		} else {
-			console.log('Click Outside');
-		}
-	}
+    @ViewChild('testButton', { static: false }) testButton: ElementRef;
+
+    @HostListener('document:click', ['$event']) onClick(event) {
+        if (this.testButton.nativeElement.contains(event.target)) {// or some similar check
+            console.log('Click Inside');
+        } else {
+            console.log('Click Outside');
+        }
+    }
 
 }
